fd2f5575daba2323b5e6e56a02e13fe220feba8f
Nina Hawari <nina.hawari@gmail.com>
2022-11-01 22:19:24 +0200

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(drivers) {
+    return [drivers[0],drivers[1]];
+    
+}
+
+console.log(returnFirstTwoDrivers)
+
+
+const returnLastTwoDrivers = (drivers) => {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+const createFareMultiplier = (fare) => {
+    return function(fare) {
+    return fare*fare;
+}
+}
+
+const fareDoubler = (createFareMultiplier) => {
+    return createFareMultiplier*2;
+    }
+    
+const fareTripler = (createFareMultiplier) => {
+    return createFareMultiplier*3;
+}
+
+function selectDifferentDrivers(drivers,fn) {
+   return fn(drivers);
+}
